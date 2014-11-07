d277c286def538f94bf1cb68f5623e25a24363ee
grooney <grooney@stanford.edu>
2014-11-06 16:27:38 -0800

    Gino - Test Commit

--- src/quizsite/Dummy.java
+++ src/quizsite/Dummy.java
@@ -8,7 +8,7 @@ public class Dummy {
 		// TODO Auto-generated method stub
 		ArrayList<String> test = new ArrayList<String>();
 		
-		String rightMessage = "Right message 4. Matt was here.";
+		String rightMessage = "Right message 4. Matt was here. So was Gino";
 		
 		test.add("Wrong message 1");
 		test.add("Wrong message 2");
