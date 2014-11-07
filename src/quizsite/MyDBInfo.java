41b12503a85b78173a6adeeeee29a4db6d559308
Dan Duncan <danduncan2010@gmail.com>
2014-11-07 02:45:39 -0800

    Added MyDBInfo.java (I hope)

---
+++ src/quizsite/MyDBInfo.java
@@ -0,0 +1,10 @@
+package quizsite;
+
+public class MyDBInfo {
+	
+	public static final String MYSQL_USERNAME = "ccs108duncand";
+	public static final String MYSQL_PASSWORD = "danpw"; // Original password was aepheiyu
+	public static final String MYSQL_DATABASE_SERVER = "mysql-user-master.stanford.edu";
+	public static final String MYSQL_DATABASE_NAME = "c_cs108_duncand";
+
+}
