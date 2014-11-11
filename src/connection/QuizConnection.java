433a28888d4a1edcb5a7b945462e26665d354167
grooney <grooney@stanford.edu>
2014-11-10 19:11:59 -0800

    working on QuizConnection class

--- src/connection/QuizConnection.java
+++ src/connection/QuizConnection.java
@@ -1,5 +1,10 @@
 package connection;
 
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+import Quiz.QuestionType;
+
 import quizsite.DatabaseConnection;
 
 //this class retrieves and updates quiz information in database
@@ -7,14 +12,38 @@ public class QuizConnection {
 	private String stringdelimiter = "&&&";
 	private String arraydelimiter = "@";
 	private DatabaseConnection db;
+	private ArrayList<QuestionType> questiontypes = new ArrayList<QuestionType>();
 	
 	//ultimately want to make string and array inputs from site manager
-	public QuizConnection(DatabaseConnection DB){
+	public QuizConnection(DatabaseConnection DB, ArrayList<QuestionType> QuestionTypes){
 		db = DB;
+		questiontypes = QuestionTypes;
 	}
 	
-	public void getAttribute(String field, Integer ID){
-		
+	public Object getAttribute(String field, Integer ID){
+		String query;
+		try{
+			if (field.equals("questions")){
+				for (int i = 0; i < questiontypes.size(); i++){
+					query = "SELECT * FROM "+questiontypes.get(i).tableName +" WHERE (id = "+ID+")";
+					
+				
+				}
+				 
+			} else {
+				query = "SELECT " + field + "  FROM quizzes WHERE (id = "+ID+")";
+				ResultSet rs = db.executeQuery(query);
+			
+				String fieldvalue = "";
+				while(rs.next()) {
+					fieldvalue = rs.getString(field);
+			}
+
+			return fieldvalue;
+		}
+		} catch (Exception e){
+			e.printStackTrace();
+		}
 	}
 
 	
