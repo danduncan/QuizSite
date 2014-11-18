5bb009405a1369827cea5247eba88af2ee9a0d6e
Dan Duncan <danduncan2010@gmail.com>
2014-11-17 20:24:16 -0800

    Add ScoreManager

--- src/Quiz/Score.java
+++ src/Quiz/Score.java
@@ -5,14 +5,15 @@ public class Score {
 	public int quizid = 0;
 	public String date = null;
 	public int score = 0;
-	public int timeInSeconds = 0;
+	public int time = 0; // Completion time, in seconds
 	
 	public Score(int userid, int quizid, int score, int timeInSeconds, String date) {
 		this.userid = userid;
 		this.quizid = quizid;
 		this.score = score;
-		this.timeInSeconds = timeInSeconds;
+		this.time = timeInSeconds;
 		this.date = date;
 		
 	}
+	
 }
