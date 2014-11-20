3187d0a7f9dfab05797fc1c81d3a0618b06fb1eb
Dan Duncan <danduncan2010@gmail.com>
2014-11-20 08:02:02 -0800

    Update SiteManager. Database is now read from and/or written to every time a value is needed. Local instance variables still exist, but aren't used in this implementation. Excess code has been commented out as well. If you need a method added back to the SiteManager, let me know and I'll add it and make sure it is working.

--- src/quizsite/SiteManager.java
+++ src/quizsite/SiteManager.java
@@ -13,13 +13,15 @@ public class SiteManager {
 	private static String colNextQuizID = "nextquizid";
 	private static String colNextQuestionID = "nextquestionid";
 	private static String colNextMessageID = "nextmessageid";
+	private static String colNextQuizTakenID = "nextquiztakenid";
+	
 	private static String colTotalUsers = "totalusers";
 	private static String colTotalQuizzes = "totalquizzes";
 	private static String colTotalQuestions = "totalquestions";
-	private static String colTotalQuizzesTaken = "totalquizzestaken";
+	//private static String colTotalQuizzesTaken = "totalquizzestaken";
 	private static String colTotalQuizzesPracticed = "totalquizzespracticed";
 	private static String colTotalFriendships = "totalfriendships";
-	private static String colTotalMessages = "totalmessages";
+	//private static String colTotalMessages = "totalmessages";
 	private static String colTotalQuizCategories = "totalquizcategories";
 	private static String colTotalQuestionCategories = "totalquestioncategories";
 	private static String colTotalAchievements = "totalachievements";
@@ -30,15 +32,16 @@ public class SiteManager {
 	private int nextQuizID = 0;
 	private int nextQuestionID = 0;
 	private int nextMessageID = 0;
+	private int nextQuizTakenID = 0;
 	
 	// Collection of site metrics
 	private int totalUsers = 0;
 	private int totalQuizzes = 0;
-	private int totalQuizzesTaken = 0;
+	//private int totalQuizzesTaken = 0;
 	private int totalQuizzesPracticed = 0;
 	private int totalQuestions = 0;
 	private int totalFriendships = 0;
-	private int totalMessages = 0;
+	//private int totalMessages = 0;
 	private int totalQuizCategories = 0;
 	private int totalQuestionCategories = 0;
 	private int totalAchievements = 0;
@@ -64,28 +67,89 @@ public class SiteManager {
 		nextQuizID = setIVar(rs, colNextQuizID);
 		nextQuestionID = setIVar(rs, colNextQuestionID);
 		nextMessageID = setIVar(rs, colNextMessageID);
+		nextQuizTakenID = setIVar(rs, colNextQuizTakenID);
+		
 		totalUsers = setIVar(rs, colTotalUsers);
 		totalQuizzes = setIVar(rs, colTotalQuizzes);
-		totalQuizzesTaken = setIVar(rs, colTotalQuizzesTaken);
+		//totalQuizzesTaken = setIVar(rs, colTotalQuizzesTaken);
 		totalQuizzesPracticed = setIVar(rs, colTotalQuizzesPracticed);
 		totalQuestions = setIVar(rs, colTotalQuestions);
 		totalFriendships = setIVar(rs, colTotalFriendships);
-		totalMessages = setIVar(rs, colTotalMessages);
+		//totalMessages = setIVar(rs, colTotalMessages);
 		totalQuizCategories = setIVar(rs, colTotalQuizCategories);
 		totalQuestionCategories = setIVar(rs, colTotalQuestionCategories);
 		totalAchievements = setIVar(rs, colTotalAchievements);
 		totalUniqueVisitors = setIVar(rs, colTotalUniqueVisitors);
 	}
 	
-	private int setIVar(ResultSet rs, String colName) {
-		int ivar = 0;
+	
+	private int readTableColumn(String colName) {
+		String query = "SELECT " + colName + " from " + this.table + ";";
+		ResultSet rs = dc.executeQuery(query);
 		try {
-			ivar = rs.getInt(colName);
+			// First check if rs is null or empty
+			if (rs == null || !rs.first()) return -1;
+			
+			// Next extract the value from the desired column
+			return setIVar(rs,colName);
+			
+		} catch (SQLException ignored) {return -1;}		
+	}
+	
+	// Read an entry from the table. Enforce that entries are always >= 0
+	private int setIVar(ResultSet rs, String colName) {
+		int ivar = -1;
+		try {
+			int iv = rs.getInt(colName);
+			
+			if (iv < 0) {
+				iv = 0;
+				updateTableColumn(colName,iv);
+			}
+			
+			ivar = iv;
 		} catch (SQLException e) {
 			System.out.println("SiteManager readTable(): Invalid column name: " + colName);
 		}
 		return ivar;
 	}
+	
+	// Update an entry in the table. Enforce that no entry is < 0.
+	private void updateTableColumn(String colName, int ivar) {
+		if (ivar < 0) ivar = 0;
+		String update = "UPDATE " + table + " SET " + colName + " = " + ivar + ";";
+		dc.executeUpdate(update);
+	}
+	
+	// Get-and-increment an IVar (i.e. the x++ operation)
+	private int IVarPlusPlus(String colName) {
+		int iv = readTableColumn(colName);
+		updateTableColumn(colName, iv + 1);
+		return iv;
+	}
+	
+	// Get-and-decrement an IVar (i.e. the x-- operation)
+	private int IVarMinusMinus(String colName) {
+		int iv = readTableColumn(colName);
+		updateTableColumn(colName, iv - 1);
+		return iv;
+	}
+
+	// Increment-and-get an IVar (i.e. the ++x operation)
+	private int PlusPlusIVar(String colName) {
+		int iv = readTableColumn(colName) + 1;
+		updateTableColumn(colName, iv);
+		return iv;
+	}
+
+	// Decrement-and-get an IVar (i.e. the --x operation)
+	private int MinusMinusIVar(String colName) {
+		int iv = readTableColumn(colName) - 1;
+		updateTableColumn(colName, iv);
+		return iv;
+	}
+		
+	
 	
 	/**
 	 * GETTING an ID for X automatically increments the nextXID and totalX fields
@@ -93,75 +157,95 @@ public class SiteManager {
 	 * @return int nextID
 	 */
 	public int popNextUserID() {
-		this.totalUsers++;
-		return this.nextUserID++;
+		// Need to increment two columns
+		this.totalUsers = PlusPlusIVar(colTotalUsers);
+		int id = IVarPlusPlus(colNextUserID);
+		this.nextUserID = id + 1;
+		return id;
 	}
 	public int popNextQuizID() {
-		this.totalQuizzes++;
-		return this.nextQuizID++;
+		// Need to increment two columns
+		this.totalQuizzes = PlusPlusIVar(colTotalQuizzes);
+		int id = IVarPlusPlus(colNextQuizID);
+		this.nextQuizID = id + 1;
+		return id;
 	}
 	public int popNextQuestionID() {
-		this.totalQuestions++;
-		return this.nextQuestionID++;
+		// Need to increment two columns
+		this.totalQuestions = PlusPlusIVar(colTotalQuestions);
+		int id = IVarPlusPlus(colNextQuestionID);
+		this.nextQuestionID = id + 1;
+		return id;
 	}
 	public int popNextMessageID() {
-		this.totalMessages++;
-		return this.nextMessageID++;
+		int id = IVarPlusPlus(colNextMessageID);
+		this.nextMessageID = id + 1;
+		return id;
 	}
 	public int popNextQuizTakenID() {
-		return ++this.totalQuizzesTaken;
+		int id = IVarPlusPlus(colNextQuizTakenID);
+		this.nextQuizTakenID = id + 1;
+		return id;
 	}
+	/*
 	public int getNextUserID() {
-		return this.nextUserID;
+		return readTableColumn(colNextUserID);
 	}
 	public int getNextQuizID() {
-		return this.nextQuizID;
+		return readTableColumn(colNextQuizID);
 	}
 	public int getNextQuestionID() {
-		return this.nextQuestionID;
+		return readTableColumn(colNextQuestionID);
 	}
 	public int getNextMessageID() {
-		return this.nextMessageID;
+		return readTableColumn(colNextMessageID);
 	}
-	
+	public int getNextQuizTakenID() {
+		return readTableColumn(colNextQuizTakenID);
+	}
+	*/
 	
 	
 	// Getters for other major ivars. These do not modify any stored values
+	/*
 	public int gettotalUsers() {
-		return totalUsers;
+		return readTableColumn(colTotalUsers);
 	}
 	public int getTotalQuizzes() {
-		return totalQuizzes;
+		return readTableColumn(colTotalQuizzes);
 	}
 	public int getTotalQuizzesTaken() {
-		return totalQuizzesTaken;
+		return readTableColumn(colNextQuizTakenID);
 	}
 	public int getTotalQuizzesPracticed() {
-		return totalQuizzesPracticed;
+		return readTableColumn(colTotalQuizzesPracticed);
 	}
 	public int getTotalQuestions() {
-		return totalQuestions;
+		return readTableColumn(colTotalQuestions);
 	}
 	public int getTotalFriendships() {
-		return totalFriendships;
+		return readTableColumn(colTotalFriendships);
 	}
 	public int getTotalMessages() {
-		return totalMessages;
+		return readTableColumn(nextMessageID);
 	}
 	public int getTotalQuizCategories() {
-		return totalQuizCategories;
+		return readTableColumn(colTotalQuizCategories);
 	}
 	public int getTotalQuestionCategories() {
-		return totalQuestionCategories;
+		return readTableColumn(colTotalQuestionCategories);
 	}
 	public int getTotalAchievements() {
-		return totalAchievements;
+		return readTableColumn(colTotalAchievements);
 	}
 	public int getTotalUniqueVisitors() {
-		return totalUniqueVisitors;
+		return readTableColumn(colTotalUniqueVisitors);
 	}
+	*/
 	
-	// Functions to increment or decrement ivars
+	// Functions to add or delete site objects
+	// NOTE: These adders are obsolete. Use "popNext___()" to add an object 
+	/*
 	public void addUser() {
 		nextUserID++;
 		totalUsers++;
@@ -172,69 +256,68 @@ public class SiteManager {
 	}
 	public void addQuestion() {
 		nextQuestionID++;
-		totalQuestions++;
+		//totalQuestions++;
 	}
 	public void addMessage() {
 		nextMessageID++;
-		totalMessages++;
-	}
-	public void deleteUser() {
-		totalUsers--;
-	}
-	public void deleteQuiz() {
-		totalQuizzes--;
-	}
-	public void deleteQuestion() {
-		totalQuestions--;
+		//totalMessages++;
 	}
 	public void addQuizTaken() {
-		totalQuizzesTaken++;
+		//totalQuizzesTaken++;
+	}
+	*/
+	public void deleteUser() {
+		totalUsers = MinusMinusIVar(colTotalUsers);
+	}
+	public void deleteQuiz() {
+		totalQuizzes = MinusMinusIVar(colTotalQuizzes);
+	}
+	public void deleteQuestion() {
+		totalQuestions = MinusMinusIVar(colTotalQuestions);
 	}
 	public void addQuizPracticed() {
-		totalQuizzesPracticed++;
+		this.totalQuizzesPracticed = PlusPlusIVar(colTotalQuizzesPracticed);
 	}
 	public void addFriendship() {
-		totalFriendships++;
+		totalFriendships = PlusPlusIVar(colTotalFriendships);
 	}
 	public void addQuizCategory() {
-		totalQuizCategories++;
+		totalQuizCategories = PlusPlusIVar(colTotalQuizCategories);
 	}
 	public void addQuestionCategory() {
-		totalQuestionCategories++;
+		totalQuestionCategories = PlusPlusIVar(colTotalQuestionCategories);
 	}
 	public void addAchievement() {
-		totalAchievements++;
+		totalAchievements = PlusPlusIVar(colTotalAchievements);
 	}
 	public void addUniqueVisitor() {
-		totalUniqueVisitors++;
+		totalUniqueVisitors = PlusPlusIVar(colTotalUniqueVisitors);
 	}
 	
 	// Write all data to server
+	// Current implementation of SiteManager always stores the most recent update in the database, so
+	// this update could potentially corrupt the database by saving old information
 	public void updateDatabase() {
-		updateIVar(colNextUserID,nextUserID);
-		updateIVar(colNextQuizID,nextQuizID);
-		updateIVar(colNextQuestionID,nextQuestionID);
-		updateIVar(colNextMessageID,nextMessageID);
-		updateIVar(colTotalUsers,totalUsers);
-		updateIVar(colTotalQuizzes,totalQuizzes);
-		updateIVar(colTotalQuestions,totalQuestions);
-		updateIVar(colTotalQuizzesTaken,totalQuizzesTaken);
-		updateIVar(colTotalQuizzesPracticed,totalQuizzesPracticed);
-		updateIVar(colTotalFriendships,totalFriendships);
-		updateIVar(colTotalMessages,totalMessages);
-		updateIVar(colTotalQuizCategories,totalQuizCategories);
-		updateIVar(colTotalQuestionCategories,totalQuestionCategories);
-		updateIVar(colTotalAchievements,totalAchievements);
-		updateIVar(colTotalUniqueVisitors,totalUniqueVisitors);
+		return;
+//		updateTableColumn(colNextUserID,nextUserID);
+//		updateTableColumn(colNextQuizID,nextQuizID);
+//		updateTableColumn(colNextQuestionID,nextQuestionID);
+//		updateTableColumn(colNextMessageID,nextMessageID);
+//		updateTableColumn(colNextQuizTakenID,nextQuizTakenID);
+//		updateTableColumn(colTotalUsers,totalUsers);
+//		updateTableColumn(colTotalQuizzes,totalQuizzes);
+//		updateTableColumn(colTotalQuizzesPracticed,totalQuizzesPracticed);
+//		updateTableColumn(colTotalFriendships,totalFriendships);
+//		updateTableColumn(colTotalQuizCategories,totalQuizCategories);
+//		updateTableColumn(colTotalQuestionCategories,totalQuestionCategories);
+//		updateTableColumn(colTotalAchievements,totalAchievements);
+//		updateTableColumn(colTotalUniqueVisitors,totalUniqueVisitors);
+
+		//updateTableColumn(colTotalQuestions,totalQuestions);
+		//updateTableColumn(colTotalQuizzesTaken,totalQuizzesTaken);
+		//updateTableColumn(colTotalMessages,totalMessages);
 	}
-	
-	
-	private void updateIVar(String colName, int ivar) {
-		String update = "UPDATE " + table + " SET " + colName + " = " + ivar + ";";
-		dc.executeUpdate(update);
-	}
-	
-	
+		
 	@Override
 	public String toString() {
 		String ls = System.getProperty("line.separator");
@@ -244,17 +327,51 @@ public class SiteManager {
 		sb.append("\tnextQuizID: " + nextQuizID + ls);
 		sb.append("\tnextQuestionID: " + nextQuestionID + ls);
 		sb.append("\tnextMessageID: " + nextMessageID + ls);
+		sb.append("\tnextQuizTakenID " + nextQuizTakenID + ls);
 		sb.append("\ttotalUsers: " + totalUsers + ls);
 		sb.append("\ttotalQuizzes: " + totalQuizzes + ls);
-		sb.append("\ttotalQuizzesTaken: " + totalQuizzesTaken + ls);
+		//sb.append("\ttotalQuizzesTaken: " + totalQuizzesTaken + ls);
 		sb.append("\ttotalQuizzesPracticed: " + totalQuizzesPracticed + ls);
 		sb.append("\ttotalQuestions: " + totalQuestions + ls);
 		sb.append("\ttotalFriendships: " + totalFriendships + ls);
-		sb.append("\ttotalMessages: " + totalMessages + ls);
+		//sb.append("\ttotalMessages: " + totalMessages + ls);
 		sb.append("\ttotalQuizCategories: " + totalQuizCategories + ls);
+		sb.append("\totalQuestionCategories: " + totalQuestionCategories + ls);
 		sb.append("\ttotalAchievements: " + totalAchievements + ls);
 		sb.append("\ttotalUniqueVisitors: " + totalUniqueVisitors + ls);
 		
 		return sb.toString();
+	}
+	
+	// For testing only
+	public static void main(String[] args) {
+		DatabaseConnection dc = new DatabaseConnection();
+		SiteManager sm = new SiteManager(dc);
+		
+		// Test that sm matches the database after many updates		
+		System.out.println("Quiz ID: " + sm.popNextQuizID());
+		System.out.println("Question ID 1: " + sm.popNextQuestionID());
+		System.out.println("Question ID 2: " + sm.popNextQuestionID());
+		System.out.println("Quiz Taken ID: " + sm.popNextQuizTakenID());
+		System.out.println("User ID 1: " +  sm.popNextUserID());
+		System.out.println("Message ID: " + sm.popNextMessageID());
+		System.out.println("User ID 2: " +  sm.popNextUserID());
+		System.out.println("Total users: " + sm.totalUsers + "; Now, delete two users...");
+		sm.deleteUser();
+		sm.deleteUser();
+		System.out.println("Total users: " + sm.totalUsers);
+		System.out.println("User ID 3: " +  sm.popNextUserID());
+		sm.deleteQuiz(); sm.deleteQuestion(); sm.addQuizPracticed();
+		sm.addFriendship(); sm.addFriendship(); sm.addFriendship(); 
+		sm.addQuizCategory(); sm.addQuestionCategory();
+		sm.addAchievement(); sm.addUniqueVisitor();
+		sm.deleteQuiz(); sm.deleteQuestion(); sm.addQuizPracticed();
+		sm.addFriendship(); sm.addFriendship(); sm.addFriendship(); 
+		sm.addQuizCategory(); sm.addQuestionCategory();
+		sm.addAchievement(); sm.addUniqueVisitor();
+		
+		System.out.println(sm);
+		sm.readTable();
+		System.out.println(sm);
 	}
 }
