05e88e6af983045c3c280eb282a347a908ea4408
Dan Duncan <danduncan2010@gmail.com>
2014-12-03 21:39:30 -0800

    Add method to Friend to query recent friends activity from database

--- src/users/Friend.java
+++ src/users/Friend.java
@@ -100,6 +100,7 @@ public class Friend {
 		
 		return activity;
 	}
+	
 	public static int getNumRequests(DatabaseConnection dc, Integer userid) throws SQLException{
 		String query =  "select replied from messages where type = 0 AND receiverid = " + userid; 
 		ResultSet rs = dc.executeQuery(query);
@@ -120,4 +121,62 @@ public class Friend {
 		}
 		return friendIDs;
 	}
+	
+	
+	// This resultset contains recent activity, ordered by date (most recent first)
+	// The database only returns "limit" entries
+	// limit = null or limit <= 0 defaults to infinite limit
+	// userid is the id number of the user whose friends' activity you want
+	// Format of ResultSet:
+	// 	Columns:	| userid | username | date | achType | achName | achIcon | quizid | quizname | quizscore |
+	// Three kinds of events are included, and for each event, non-relevant columns are null
+	// 		User earned an achievement: quizid == quizname == quizscore == null
+	//		User created a quiz:		achType == achName == achIcon == quizscore == null
+	//		User took a quiz: 			achType == achName == achIcon == null
+	// Query the null values to determine what kind of event each entry was
+	private static ResultSet getTheBeast(Integer userid, Integer limit, DatabaseConnection dc) {
+		if (userid == null || dc == null) return null;
+		
+		String theBeast = getBeastlyQuery(userid,limit);
+		if (theBeast == null || theBeast.isEmpty()) return null;
+		
+		ResultSet rs = null;
+		rs = dc.executeQuery(theBeast);
+		if (rs == null) {
+			System.out.println("Friend.getTheBeast(): Beastly query yielded a null ResultSet. Verify the query!");
+		}
+		return rs;
+	}
+	
+	// Get beastly SQL query that lists recent friends' activity
+	// Only gets n most recent items, where n = limit
+	// Sorted in descending order by date, regardless of what kind of activity
+	private static String getBeastlyQuery(Integer userid, Integer limit) {
+		String limitStr = "";
+		if (userid == null || userid < 1) return "";
+		if (limit != null && limit > 0) limitStr = " LIMIT " + limit;
+		
+		StringBuilder theBeast = new StringBuilder();
+		
+		theBeast.append("SELECT userid,username,date,achType,achName,achIcon,quizid,quizname,quizscore FROM (select friend2 from friends where friend1=" + userid + ") foo JOIN (");
+		theBeast.append("select achievements.userid,users.username,achievements.dateachieved as date,achievements.type as achType, achievementTypes.name as achName, achievementTypes.icon as achIcon, null as quizid, null as quizname, null as quizscore from achievements ");
+		theBeast.append("JOIN achievementTypes ON (achievements.type=achievementTypes.id) JOIN users ON (users.id = achievements.userid) UNION ALL ");
+		theBeast.append("select quizzes.authorid as userid, users.username, quizzes.datemade as date, null, null, null, quizzes.id as quizid, quizzes.name as quizname, null from ");
+		theBeast.append("quizzes JOIN users ON (quizzes.authorid = users.id) UNION ALL ");
+		theBeast.append("select users.id as userid, users.username, quizzesTaken.datetaken as date, null, null, null, quizzesTaken.quizid as quizid, quizzes.name as quizname, quizzesTaken.score as quizscore FROM quizzesTaken ");
+		theBeast.append("JOIN users ON (quizzesTaken.userid = users.id) JOIN quizzes ON (quizzesTaken.quizid = quizzes.id) ");
+		theBeast.append(") bar where foo.friend2 = bar.userid ");
+		theBeast.append("ORDER BY date DESC" + limitStr + ";");
+		
+		return theBeast.toString();
+	}
+	
+	public static void main(String[] args) {
+		Integer userid = 4;
+		Integer limit = 15;
+		
+		System.out.println(":::THE BEAST:::");
+		System.out.println(getBeastlyQuery(userid,limit));
+	}
+	
 }
